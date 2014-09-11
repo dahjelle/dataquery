9a83315ce3688b0063300b2601a6b6f7507683df
David Alan Hjelle <dahjelle+github.com@thehjellejar.com>
2014-09-11 17:26:29 -0500

    Refactor.

--- src/datascript/core.cljs
+++ src/datascript/core.cljs
@@ -25,14 +25,10 @@
 ;;;;;;;;;; Searching
 
 
-(defn- slice-it [index search callback]
-  (callback (let [search-start search
-        search-stop  (mapv #(if (nil? %) "\uffff" %) search)]
-    (vec (vals (subseq index >= search-start <= search-stop))))))
-
 (defprotocol ISearch
   (-search [data pattern callback])
-  (add-record [data e a v]))
+  (add-record [data e a v])
+  (slice-it [data index search callback]))
 
 (defrecord DB [eav aev ave]
   Object
@@ -40,22 +36,27 @@
     (pr-str* this))
 
   ISearch
-  (-search [_ [e a v] callback]
+  (-search [db [e a v] callback]
     (case-tree [e a (some? v)] [
-      (slice-it eav [e a v] callback)                ;; e a v
-      (slice-it eav [e a nil] callback)              ;; e a _
-      (slice-it eav [e nil nil] (fn [data]
+      (slice-it db "eav" [e a v] callback)                ;; e a v
+      (slice-it db "eav" [e a nil] callback)              ;; e a _
+      (slice-it db "eav" [e nil nil] (fn [data]
         (callback (filter #(= v (.-v %)) data))))    ;; e _ v
-      (slice-it eav [e nil nil] callback)            ;; e _ _
-      (slice-it ave [a v nil] callback)              ;; _ a v
-      (slice-it ave [a nil nil] callback)            ;; _ a _
+      (slice-it db "eav" [e nil nil] callback)            ;; e _ _
+      (slice-it db "ave" [a v nil] callback)              ;; _ a v
+      (slice-it db "ave" [a nil nil] callback)            ;; _ a _
       (callback (filter #(= v (.-v %)) eav))         ;; _ _ v
-      (callback eav)]))                              ;; _ _ _
+      (callback "eav")]))                              ;; _ _ _
   (add-record [_ e a v]
     (DB.
       (assoc eav [e a v] [e a v])
       (assoc aev [a e v] [e a v])
-      (assoc ave [a v e] [e a v]))))
+      (assoc ave [a v e] [e a v])))
+  (slice-it [db index search callback]
+    (callback (let [search-start search
+                    search-stop  (mapv #(if (nil? %) "\uffff" %) search)
+                    index        ((keyword index) db)]
+      (vec (vals (subseq index >= search-start <= search-stop)))))))
 
 (defn init-db []
   (DB. (sorted-map) (sorted-map) (sorted-map)))
