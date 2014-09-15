d2930de636001c2817d67c3836f6c3b888f56705
David Alan Hjelle <dahjelle+github.com@thehjellejar.com>
2014-09-15 08:52:32 -0500

    Fix comments.

--- src/datascript/core.cljs
+++ src/datascript/core.cljs
@@ -15,15 +15,15 @@
   ISearch
   (-search [db [e a v] callback]
     (case-tree [e a (some? v)] [
-      (slice-it db "eav" [e a v] callback)                ;; e a v
-      (slice-it db "eav" [e a nil] callback)              ;; e a _
+      (slice-it db "eav" [e a v] callback)           ;; e a v
+      (slice-it db "eav" [e a nil] callback)         ;; e a _
       (slice-it db "eav" [e nil nil] (fn [data]
         (callback (filter #(= v (.-v %)) data))))    ;; e _ v
-      (slice-it db "eav" [e nil nil] callback)            ;; e _ _
-      (slice-it db "ave" [a v nil] callback)              ;; _ a v
-      (slice-it db "ave" [a nil nil] callback)            ;; _ a _
+      (slice-it db "eav" [e nil nil] callback)       ;; e _ _
+      (slice-it db "ave" [a v nil] callback)         ;; _ a v
+      (slice-it db "ave" [a nil nil] callback)       ;; _ a _
       (callback (filter #(= v (.-v %)) eav))         ;; _ _ v
-      (callback "eav")]))                              ;; _ _ _
+      (callback "eav")]))                            ;; _ _ _
   (add-record [_ e a v]
     (DB.
       (assoc eav (mapv str [e a v]) (to-array [e a v]))
