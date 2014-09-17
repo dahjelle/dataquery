3d5abc6dac454834fe43912c34c5f69fe3c80330
David Alan Hjelle <dahjelle+github.com@thehjellejar.com>
2014-09-17 14:47:56 -0500

    Refactor.

--- src/datascript/core.cljs
+++ src/datascript/core.cljs
@@ -3,11 +3,9 @@
     [datascript :refer [combine-cmp case-tree]]))
 
 (defprotocol ISearch
-  (-search [data pattern callback])
-  (add-record [data e a v])
-  (from-index [data index search callback]))
+  (-search [data pattern callback]))
 
-(defrecord DB [eav aev ave]
+(defrecord DB [db search-eav search-ave]
   Object
   (toString [this]
     (pr-str* this))
@@ -15,26 +13,36 @@
   ISearch
   (-search [db [e a v] callback]
     (case-tree [e a (some? v)] [
-      (from-index db "eav" [e a v] callback)         ;; e a v
-      (from-index db "eav" [e a nil] callback)       ;; e a _
-      (from-index db "eav" [e nil nil] (fn [data]
+      (search-eav [e a v] callback)                  ;; e a v
+      (search-eav [e a nil] callback)                ;; e a _
+      (search-eav [e nil nil] (fn [data]
         (callback (filter #(= v (.-v %)) data))))    ;; e _ v
-      (from-index db "eav" [e nil nil] callback)     ;; e _ _
-      (from-index db "ave" [a v nil] callback)       ;; _ a v
-      (from-index db "ave" [a nil nil] callback)     ;; _ a _
-      (callback (filter #(= v (.-v %)) eav))         ;; _ _ v
-      (callback "eav")]))                            ;; _ _ _
-  (add-record [_ e a v]
-    (DB.
-      (assoc eav (mapv str [e a v]) (to-array [e a v]))
-      (assoc aev (mapv str [a e v]) (to-array [e a v]))
-      (assoc ave (mapv str [a v e]) (to-array [e a v]))))
-  (from-index [db index search callback]
+      (search-eav [e nil nil] callback)              ;; e _ _
+      (search-ave [a v nil] callback)                ;; _ a v
+      (search-ave [a nil nil] callback)              ;; _ a _
+      (callback (filter #(= v (.-v %)) "eav"))       ;; _ _ v FIXME!
+      (callback "eav")]))                            ;; _ _ _ FIXME!
+)
+
+(defn search-index [index]
+  (fn [search callback]
     (callback (let [search-start search
-                    search-stop  (mapv #(if (nil? %) "\uffff" %) search)
-                    index        ((keyword index) db)]
+                    search-stop  (mapv #(if (nil? %) "\uffff" %) search)]
       (vec (vals (subseq index >= search-start <= search-stop)))))))
 
+(defn add-record [db e a v]
+  (let [record    (to-array [e a v])
+        indexes   (:db db)
+        ave       (assoc (:ave indexes) (mapv str [a v e]) record)
+        eav       (assoc (:eav indexes) (mapv str [e a v]) record)]
+    (DB. (hash-map :eav eav :ave ave)
+         (search-index eav)
+         (search-index ave))))
+
 (defn init-db []
-  (DB. (sorted-map) (sorted-map) (sorted-map)))
+  (let [ave          (sorted-map)
+        eav          (sorted-map)]
+    (DB. (hash-map :eav eav :ave ave)
+         (search-index eav)
+         (search-index ave))))
 
